e2b1287f507bba01f8eb47b4593f3f4f7881e4b9
scottgrayart <scott@scottgray.art>
2025-10-07 22:00:06 -0700

    fix type and some type checking

--- main.ts
+++ main.ts
@@ -34,38 +34,40 @@ export default class PlayWithCheckboxs extends Plugin {
 		this.registerMarkdownCodeBlockProcessor('checkboxs', async (source, el, ctx) => {
 			try {
 				const def = parseYaml(source);
-				console.log('def'); console.log(def);
-				const currentFile: TFile = this.app.workspace.getActiveFile();
+				const currentFile: TFile | null = this.app.workspace.getActiveFile();
 				let currentFm = null;
-				await this.app.fileManager.processFrontMatter(currentFile, fm => { currentFm = fm });
-				console.log(currentFm)
-				el.createEl('h2', { text: def.title });
-				const boxContainer = el.createDiv({ cls: 'el-ul' });
-				const listContainer = boxContainer.createEl('ul', { cls: 'contains-task-list has-list-bullet' });
-				for (let item of def.boxesp) {
-					for (let property in item) {
-						const li = listContainer.createEl('li', { cls: 'task-list-item' });
-						const cb = li.createEl("input", { type: "checkbox", cls:'task-list-item-checkbox' });
-						cb.setAttribute('_tag', property);
-						li.appendText(property);
-						cb.checked = currentFm.tags.includes(property);
-						cb.onchange = async () => {
-							console.log(currentFile);
-							const tag = cb.getAttribute('_tag');
-							if (currentFile) {
-								await this.app.fileManager.processFrontMatter(currentFile, fm => {
-									console.log(fm)
-									if (fm.tags) {
-										if (!fm.tags.includes(tag))
-											fm.tags.push(tag)
-										else
-											fm.tags = fm.tags.filter(val => val !== tag);
-									}
-									console.log(fm)
-								});
-							}
-						};
+				if (currentFile) {
+					await this.app.fileManager.processFrontMatter(currentFile, fm => { currentFm = fm });
+					console.log(currentFm)
+					el.createEl('h2', { text: def.title });
+					const boxContainer = el.createDiv({ cls: 'el-ul' });
+					const listContainer = boxContainer.createEl('ul', { cls: 'contains-task-list has-list-bullet' });
+					for (let item of def.boxes) {
+						for (let property in item) {
+							const li = listContainer.createEl('li', { cls: 'task-list-item' });
+							const cb = li.createEl("input", { type: "checkbox", cls:'task-list-item-checkbox' });
+							cb.setAttribute('_tag', property);
+							li.appendText(property);
+							cb.checked = (currentFm !== null && currentFm.tags.includes(property));
+							cb.onchange = async () => {
+								console.log(currentFile);
+								const tag = cb.getAttribute('_tag');
+								if (currentFile) {
+									await this.app.fileManager.processFrontMatter(currentFile, fm => {
+										console.log(fm)
+										if (fm.tags) {
+											if (!fm.tags.includes(tag))
+												fm.tags.push(tag)
+											else
+												fm.tags = fm.tags.filter(val => val !== tag);
+										}
+										console.log(fm)
+									});
+								}
+							};
+						}
 					}
+
 				}
 			} finally {
 				console.log('All good!')
